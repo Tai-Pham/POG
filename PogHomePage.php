6c3faaebba0695bd1d651543327afdcc230e0976
Calvin Luong <48378030+calvin-luong@users.noreply.github.com>
2020-10-05 18:09:27 -0700

    Add files via upload

---
+++ PogHomePage.php
@@ -0,0 +1,58 @@
+<?php
+echo <<<_END
+<style>
+h1 {text-align: center;}
+p {text-align: center;}
+</style>
+<head>
+<title>Pog</title>
+</head>
+<body>
+<h1> <span style='color:#C5DBFF;font-family:"Comic Sans MS"; font-size:100px'>POG</h1>
+<body style="background-color:#64A0FF;">
+_END;
+
+
+require_once 'login.php';
+$conn = new mysqli($hn, $un, $pw, $db);
+if ($conn->connect_error) die (error());
+
+$query = "SELECT * FROM videos";
+$result = $conn->query($query);
+if(!$result) die (error());
+
+$rows = $result->num_rows;
+for ($i = 0; $i < $rows; $i++)
+{
+	$result->data_seek($i);
+	$row = $result->fetch_array(MYSQLI_ASSOC);
+	$fileLocation = $row['videoLocation'];
+	echo <<<_END
+		<img src="$fileLocation"> <br> <br>
+_END;
+}
+
+// Error msg
+function error()
+{
+  echo "░░░░░░░░░▄░░░░░░░░░░░░░░▄░░░░<br>";
+  echo "░░░░░░░░▌▒█░░░░░░░░░░░▄▀▒▌░░░<br>";
+  echo "░░░░░░░░▌▒▒█░░░░░░░░▄▀▒▒▒▐░░░<br>";
+  echo "░░░░░░░▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐░░░<br>";
+  echo "░░░░░▄▄▀▒░▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐░░░<br>";
+  echo "░░░▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌░░░ <br>";
+  echo "░░▐▒▒▒▄▄▒▒▒▒░░░▒▒▒▒▒▒▒▀▄▒▒▌░░<br>";
+  echo "░░▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐░░<br>";
+  echo "░▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄▌░<br>";
+  echo "░▌░▒▄██▄▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒▌░<br>";
+  echo "▀▒▀▐▄█▄█▌▄░▀▒▒░░░░░░░░░░▒▒▒▐░<br>";
+  echo "▐▒▒▐▀▐▀▒░▄▄▒▄▒▒▒▒▒▒░▒░▒░▒▒▒▒▌<br>";
+  echo "▐▒▒▒▀▀▄▄▒▒▒▄▒▒▒▒▒▒▒▒░▒░▒░▒▒▐░<br>";
+  echo "░▌▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒░▒░▒░▒░▒▒▒▌░<br>";
+  echo "░▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▄▒▒▐░░<br>";
+  echo "░░▀▄▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▄▒▒▒▒▌░░<br>";
+  echo "░░░░▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀░░░<br>";
+  echo "░░░░░░▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀░░░░░<br>";
+  echo "░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▀▀░░░░░░░░<br>";
+}
+?>
