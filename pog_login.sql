c106df181447d081ad89bd3353d1d42366e6fa29
Calvin Luong <48378030+calvin-luong@users.noreply.github.com>
2020-10-24 12:11:48 -0700

    updated tables

--- pog_login.sql
+++ pog_login.sql
@@ -23,14 +23,14 @@ DROP TABLE IF EXISTS `login`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `login` (
-  `userID` varchar(255) NOT NULL,
+  `userID` int NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `hash` varchar(255) DEFAULT NULL,
-  `salt` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `password` varchar(255) NOT NULL,
+  `salt1` varchar(255) NOT NULL,
+  `salt2` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -51,10 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-02 13:33:55
-
-CREATE TABLE `Account` (
-	`followers` int(255) NOT NULL,
-	`following` int(45) NOT NULL,
-	PRIMARY KEY (`userID`)
-)
+-- Dump completed on 2020-10-24 12:11:03
