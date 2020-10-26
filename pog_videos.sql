ca37ca502399c159741b2a3070e7e8db48a83249
Tai Pham <54519761+Tai-Pham@users.noreply.github.com>
2020-10-25 18:27:49 -0700

    Update pog_videos.sql

--- pog_videos.sql
+++ pog_videos.sql
@@ -32,10 +32,11 @@ DROP TABLE IF EXISTS `videos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `videos` (
+  `videoID` int NOT NULL AUTO_INCREMENT,
   `videoLocation` varchar(255) NOT NULL,
   `creator` varchar(45) NOT NULL,
   `title` varchar(45) NOT NULL,
-  PRIMARY KEY (`videoLocation`,`creator`,`title`)
+  PRIMARY KEY (`videoID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
