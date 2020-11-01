8c07096688fa18c0afbdd8df8656b3589af79c82
Calvin Luong <48378030+calvin-luong@users.noreply.github.com>
2020-11-01 14:25:29 -0800

    Add files via upload

--- PogHomePage.php
+++ PogHomePage.php
@@ -12,27 +12,32 @@ p {text-align: center;}
 <body style="background-color:#64A0FF;">
 _END;
 
+// Setting time-out for session
+ini_set('session.gc_maxlifetime', 60*60*24);
 
+session_start();
+
+// Checks if a username is inputted
+if (!isset($_SESSION['username']))
+{
+	die ("Please <a href='PogLogin.php'> click here </a> to log in.");
+}
+
+// Session Security
+if (!isset($_SESSION['check']) && $_SESSION['check'] != hash('ripemd128', $_SERVER['REMOTE_ADDR'] .$_SERVER['HTTP_USER_AGENT']))
+{
+	different_user();
+}
+
+if (!isset($_SESSION['initiated'])) 
+{
+	session_regenerate_id();
+	$_SESSION['initiated'] = 1;
+}
 require_once 'login.php';
 $conn = new mysqli($hn, $un, $pw, $db);
 if ($conn->connect_error) die (error());
 
-$query = "SELECT * FROM videos";
-$result = $conn->query($query);
-if(!$result) die (error());
-
-$rows = $result->num_rows;
-for ($i = 0; $i < $rows; $i++)
-{
-	$result->data_seek($i);
-	$row = $result->fetch_array(MYSQLI_ASSOC);
-	$fileLocation = $row['videoLocation'];
-	echo <<<_END
-		<img src="$fileLocation"> <br> <br>
-_END;
-}
-
-// Error msg
 function error()
 {
   echo "░░░░░░░░░▄░░░░░░░░░░░░░░▄░░░░<br>";
