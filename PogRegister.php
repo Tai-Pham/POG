7ca8506a474fb88362799eff8b97e42ba19e7a7f
Calvin Luong <48378030+calvin-luong@users.noreply.github.com>
2020-11-17 13:22:21 -0800

    Update PogRegister.php

--- PogRegister.php
+++ PogRegister.php
@@ -1,31 +1,85 @@
 <?php
 echo <<<_END
 <style>
-body {text-align: center;}
+  body{
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+  background: #34495e;
+  text-align: center;
+}
+.box{
+  width: 300px;
+  padding: 40px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  background: #191919;
+  text-align: center;
+}
+.box h1{
+  color: white;
+  text-transform: uppercase;
+  font-weight: 500;
+}
+.box input[type = "text"],.box input[type = "password"]{
+  border:0;
+  background: none;
+  display: block;
+  margin: 20px auto;
+  text-align: center;
+  border: 2px solid #3498db;
+  padding: 14px 10px;
+  width: 200px;
+  outline: none;
+  color: white;
+  border-radius: 24px;
+  transition: 0.25s;
+}
+.box input[type = "text"]:focus,.box input[type = "password"]:focus{
+  width: 280px;
+  border-color: #2ecc71;
+}
+.box input[type = "submit"]{
+  border:0;
+  background: none;
+  display: block;
+  margin: 20px auto;
+  text-align: center;
+  border: 2px solid #2ecc71;
+  padding: 14px 40px;
+  outline: none;
+  color: white;
+  border-radius: 24px;
+  transition: 0.25s;
+  cursor: pointer;
+}
+.box input[type = "submit"]:hover{
+  background: #2ecc71;
+}
+
 </style>
-<html>
-<head>
-<title> Pog </title>
-</head>
-<body>
-<form method='post' action='PogRegister.php' enctype='multipart/form-data'>
-<h1> <span style='color:#C5DBFF;font-family:"Comic Sans MS"; font-size:100px'>POG</h1>
-<body style="background-color:#64A0FF;">
-<pre>
-<span style='color:#C5DBFF;font-family:"Comic Sans MS"; font-size:100px'> 
-Username
-<input type='text' name='username' class="form-control action-username" style="height:50px; width:300px; font-size:25px;">
-Password
-<input type = 'password' name = 'password' class="form-control action-pass" style="height:50px; width:300px; font-size:25px;">
-Repeat Password
-<input type = 'password' name = 'repeatedPassword' class="form-control action-reppass" style="height:50px; width:300px; font-size:25px;">
-Email
-<input type = 'text' name = 'email' class="form-control action-email" style="height:50px; width:300px; font-size:25px;">
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>َPog</title>
+  </head>
+  <h1> <span style='color:#C5DBFF;font-family:"Comic Sans MS"; font-size:100px;'>POG</h1>
+  <body>
+<form class="box" action="PogRegister.php" method="post">
+  <h1>Register</h1>
+  <input type="text" name="username" class="form-control action-username" placeholder="Username">
+  <input type="password" name="password" class="form-control action-pass" placeholder="Password">
+  <input type="password" name="repeatedPassword" class="form-control action-reppass" placeholder="Repeated Password">
+  <input type="password" name="email" class="form-control action-email" placeholder="Email">
+  <input type="submit" name="registerButton" class="button-back" value="Register">
+  <input type="submit" name="backButton" class="button-action" value="Back">
+</form>
 
-<input type = 'submit' value = 'Back' name = 'backButton' class="button-back" style="height:70px; width:150px; font-size:25px;">
 
-<input type = 'submit' value = 'Register' class="button-action" name = 'registerButton' style="height:70px; width:150px; font-size:25px;">
-<pre></form></body></html>
+  </body>
+</html>
 _END;
 
 require_once 'login.php';
