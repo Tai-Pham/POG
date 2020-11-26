da49174d96f1fa1cd474ccd363b3cbef6884e88d
joshrharmon <joshua.harmon@sjsu.edu>
2020-11-26 11:10:42 -0800

    Implemented full like functionality on the video page

--- PogHomePageGET.php
+++ PogHomePageGET.php
@@ -40,7 +40,7 @@ for($i = $rows - 1; $i >= 0; $i--){
         1 = liked
         2 = disliked		
     */
-    $likeQuery = "SELECT * FROM likes WHERE videoLocation=$path AND userID=$uID";
+    $likeQuery = "SELECT * FROM likes WHERE videoLocation='$path' AND userID=$uID";
     $likeResult = $conn->query($likeQuery);
     if(!$likeResult) die("Like error: " . $likeResult->error);
     
@@ -99,6 +99,84 @@ for($i = $rows - 1; $i >= 0; $i--){
 	
 	
 echo "</html>";
+
+/* 
+	Mode:
+	0: Insert a like/dislike into the likes table
+	1: Update a like/dislike into the likes table
+	2: Reflect like count in videos table
+	
+	Unset:
+	1: Unset a like
+	2: Unset a dislike
+	0: No unset action
+*/
+function like($conn, $uID, $path, $flag, $mode, $unset) {
+	if($mode == INSERT) {
+		$likeInsert = mysqli_prepare($conn, 'INSERT INTO likes(userID, videoLocation, likeFlag) VALUES(?, ?, ?)');
+		mysqli_stmt_bind_param($likeInsert, 'ssi', $uID, $path, $flag);
+		mysqli_execute($likeInsert);
+		
+		if(!$likeInsert) die (error() . $conn->error(). "<br>");
+		else {
+			mysqli_stmt_close($likeInsert);
+		}
+		like($conn, $uID, $path, $flag, UPDATE_COUNT, NULL_UNSET);
+	}
+	else if($mode == UPDATE) {
+		$likeUpdate = mysqli_prepare($conn, 'UPDATE likes SET likeFlag=? WHERE videoLocation=?');
+		mysqli_stmt_bind_param($likeUpdate, 'is', $flag, $path);
+		mysqli_execute($likeUpdate);
+		
+		if(!$likeUpdate) die (error() . $conn->error(). "<br>");
+		else {
+			mysqli_stmt_close($likeUpdate);
+		}
+		
+		if($flag == LIKE || $flag == DISLIKE) {
+			like($conn, $uID, $path, $flag, UPDATE_COUNT, NULL_UNSET);
+		}
+		else if($flag == NOLIKE_SET) {
+			if($unset == LIKE_UNSET) {
+				like($conn, $uID, $path, $flag, UPDATE_COUNT, LIKE_UNSET);
+			} 
+			else if($unset == DISLIKE_UNSET) {
+				like($conn, $uID, $path, $flag, UPDATE_COUNT, DISLIKE_UNSET);
+			}
+		}
+		else {
+			print_r("Invalid flag value! <br>");
+		}
+	}
+	else if($mode == UPDATE_COUNT) {
+		$likeCountUpdate;
+		if($flag == LIKE || $flag == NOLIKE_SET) {
+			if($unset == LIKE_UNSET) {
+				$likeCountUpdate = mysqli_prepare($conn, 'UPDATE videos SET likes = likes - 1 WHERE videoLocation=?');
+			} else {
+				$likeCountUpdate = mysqli_prepare($conn, 'UPDATE videos SET likes = likes + 1 WHERE videoLocation=?');
+			}
+		}
+		else if($flag == DISLIKE || $flag == NOLIKE_SET) {
+			if($unset == DISLIKE_UNSET) {
+				$likeCountUpdate = mysqli_prepare($conn, 'UPDATE videos SET dislikes = dislikes - 1 WHERE videoLocation=?');
+			} else {
+				$likeCountUpdate = mysqli_prepare($conn, 'UPDATE videos SET dislikes = dislikes + 1 WHERE videoLocation=?');
+			}
+		}
+		
+		mysqli_stmt_bind_param($likeCountUpdate, 's', $path);
+		mysqli_execute($likeCountUpdate);
+		
+		if(!$likeCountUpdate) die (error() . $conn->error(). "<br>");
+		else {
+			mysqli_stmt_close($likeCountUpdate);
+		}
+	}
+	else {
+		print_r("Like mode not specified!<br>");
+	}
+}
 
 function error()
 {
